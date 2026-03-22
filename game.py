4792bd33be381c483512c872c95b5a9cf3936e72
Gcmotaa <up202306287@up.pt>
2026-03-22 13:34:58 +0000

    added support for importing board states

--- game.py
+++ game.py
@@ -13,16 +13,16 @@ class Game:
         shapes = [Shape.SQUARE, Shape.TRIANGLE, Shape.CIRCLE, Shape.CIRCLE, Shape.TRIANGLE, Shape.SQUARE]
         for x, shape in enumerate(shapes):
             #Big white pieces
-            self.board.place_piece(Piece(Owner.WHITE, shape, Size.BIG),x, 0)
+            self.board.place_piece(Piece(Owner.WHITE, shape, Size.BIG),x, 5)
 
             #Small white pieces
-            self.board.place_piece(Piece(Owner.WHITE, shape, Size.SMALL),x, 1)
+            self.board.place_piece(Piece(Owner.WHITE, shape, Size.SMALL),x, 4)
 
             #Big black pieces
-            self.board.place_piece(Piece(Owner.BLACK, shape, Size.BIG),x, 5)
+            self.board.place_piece(Piece(Owner.BLACK, shape, Size.BIG),x, 0)
 
             #Small black pieces
-            self.board.place_piece(Piece(Owner.BLACK, shape, Size.SMALL),x, 4)
+            self.board.place_piece(Piece(Owner.BLACK, shape, Size.SMALL),x, 1)
 
     def print_board(self):
         shape_map = {
@@ -30,8 +30,6 @@ class Game:
             Shape.TRIANGLE: "T",
             Shape.CIRCLE: "C",
         }
-        
-        board_str = ""
         
         for y in range(self.board.size):
             row_str = ""
@@ -50,8 +48,36 @@ class Game:
                         row_str += owner + shape + " "
                     else:
                         row_str += owner.lower() + shape.lower() + " "
-            
-            board_str = row_str + "\n" + board_str
-        
-        print(board_str)
+            print(row_str)
                     
+    def load_board_from_file(self, filename: str):
+        shape_map = {
+            "S": Shape.SQUARE,
+            "T": Shape.TRIANGLE,
+            "C": Shape.CIRCLE,
+        }
+    
+        owner_map = {
+            "W": Owner.WHITE,
+            "B": Owner.BLACK,
+        }
+    
+        with open(filename, "r") as f:
+            lines = [line.strip() for line in f if line.strip()]
+    
+        # Reset board
+        self.board = Board(self.board.size)
+    
+        for y, line in enumerate(lines):
+            tokens = line.split()
+    
+            for x, token in enumerate(tokens):
+                if token == ".":
+                    continue
+                
+                owner = owner_map[token[0].upper()]
+                shape = shape_map[token[1].upper()]
+                size = Size.BIG if token.isupper() else Size.SMALL
+    
+                piece = Piece(owner, shape, size)
+                self.board.place_piece(piece, x, y)
---
+++ heuristics_test.py
@@ -0,0 +1,5 @@
+from game import Game
+
+game = Game()
+game.load_board_from_file("board1.txt")
+game.print_board()
