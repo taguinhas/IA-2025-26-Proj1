5403b7b5be8151a584abcb137f496676de22ec2c
Gcmotaa <up202306287@up.pt>
2026-03-25 10:17:28 +0000

    added tests

--- heuristics_test.py
+++ heuristics_test.py
@@ -1,7 +1,15 @@
 from game.game import Game
 from heuristics import *
 game = Game()
-game.load_board_from_file("boards/central.txt")
+game.load_board_from_file("boards/symmetrical.txt")
+#game.load_board_from_file("boards/corner.txt")
+#game.load_board_from_file("boards/upper_corner.txt")
+#game.load_board_from_file("boards/central.txt")
+#game.load_board_from_file("boards/central_no_mobility.txt")
+#game.load_board_from_file("boards/defended.txt")
+#game.load_board_from_file("boards/unsafe.txt")
+#game.load_board_from_file("boards/material.txt")
+#game.load_board_from_file("boards/generic_board.txt")
 game.print_board()
 
 score = evaluate_board(game.board)
