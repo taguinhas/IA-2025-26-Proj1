2e952f0d7697ca73dd19a59bc3c3c0b2cc3fbaca
up202304047 <up202304047@edu.fe.up.pt>
2026-04-07 22:22:15 +0100

    pygame not working

--- main.py
+++ main.py
@@ -2,6 +2,30 @@ from game.game import Game
 from game.board import Board, InvalidMoveError
 from game.piece import Piece, Player, Size, Shape
 
+import pygame
+
+pygame.init()
+WIDTH = 1000
+HEIGHT = 900
+screen = pygame.display.set_mode([WIDTH, HEIGHT])
+font = pygame.font.Font('freesansbold.ttf', 20)
+big_font = pygame.font.Font('freesansbold.ttf', 50)
+timer = pygame.time.Clock()
+fps = 60
+
+run = True
+
+while run:
+    timer.tick(fps)
+    screen.fill('dark gray')
+
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            run = False
+
+    pygame.display.flip()
+pygame.quit() 
+
 player_names = {
     Player.WHITE: "White",
     Player.BLACK: "BLACK"
