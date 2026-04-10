bd3dd0945707c40245eff31474f62cf9f1783551
sofia <sofiafurtado77@hotmail.com>
2026-04-10 23:22:36 +0100

    Added board to pygame

--- main_pygame.py
+++ main_pygame.py
@@ -3,26 +3,44 @@ import pygame
 
 # pygame setup
 pygame.init()
-screen = pygame.display.set_mode((800, 600))
+width = 1000
+height = 800
+screen = pygame.display.set_mode((width, height))
+pygame.display.set_caption('Alapo')
 clock = pygame.time.Clock()
-running = True
+fps = 30
 
+
+# draw board:
+def draw_board():
+     for i in range(18):
+        col = i%3
+        row = i//3
+        if row % 2 == 0:
+            pygame.draw.rect(screen, 'light gray', 
+            [600 - (col*200), row*100, 100, 100])
+            pygame.draw.rect(screen, 'dark gray', 
+            [700 - (col*200), row*100, 100, 100])
+        else:
+            pygame.draw.rect(screen, 'dark gray', 
+            [600 - (col*200), row*100, 100, 100])
+            pygame.draw.rect(screen, 'light gray', 
+            [700 - (col*200), row*100, 100, 100])
+
+# main pygame loop:
+running = True
 while running:
     # poll for events
     # pygame.QUIT event means the user clicked X to close your window
+    clock.tick(fps)
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
 
     # fill the screen with a color to wipe away anything from last frame
-    screen.fill((30, 30, 30))
-    # RENDER YOUR GAME HERE
+    screen.fill('gray')
+    draw_board()
 
-    pygame.draw.circle(screen, "red", (300, 200), 50)
     # flip() the display to put your work on screen
     pygame.display.flip()
-
-    clock.tick(60)  # limits FPS to 60
-
-
 pygame.quit()
